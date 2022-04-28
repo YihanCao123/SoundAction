76f9064a358a843abae8dbc9fb246bf4e09e299c
ZhongyueZhang785 <maggie785611080@yahoo.com>
2022-04-28 14:41:40 +0800

    revise bert

--- model/models.py
+++ model/models.py
@@ -29,7 +29,7 @@ class ConvBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
                                kernel_size=(3, 3), stride=(1, 1),
                                padding=(1, 1), bias=False)
-        
+
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
                                kernel_size=(3, 3), stride=(1, 1),
                                padding=(1, 1), bias=False)
@@ -38,7 +38,7 @@ class ConvBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.init_weight()
-    
+
     def init_weight(self):
         init_layer(self.conv1)
         init_layer(self.conv2)
@@ -59,13 +59,12 @@ class ConvBlock(nn.Module):
             x = x1 + x2
         else:
             raise Exception("Incorrect pooling method. ")
-        
+
         return x
 
 
 class Cnn14(nn.Module):
     def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, fmax, classes_num):
-
         super(Cnn14, self).__init__()
 
         window = 'hann'
@@ -77,20 +76,22 @@ class Cnn14(nn.Module):
 
         # Spectrogram extractor
         self.spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size,
-            win_length=window_size, window=window, center=center, pad_mode=pad_mode,
-            freeze_parameters=True
-        )
+                                                 win_length=window_size, window=window, center=center,
+                                                 pad_mode=pad_mode,
+                                                 freeze_parameters=True
+                                                 )
 
         # Logmel feature extractor
         self.logmel_extractor = LogmelFilterBank(sr=sample_rate, n_fft=window_size,
-            n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db,
-            freeze_parameters=True
-        )
+                                                 n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin,
+                                                 top_db=top_db,
+                                                 freeze_parameters=True
+                                                 )
 
         # Spec augmenter
         self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, freq_drop_width=8,
-            freq_stripes_num=2
-        )
+                                               freq_stripes_num=2
+                                               )
 
         self.bn0 = nn.BatchNorm2d(64)
 
@@ -112,21 +113,21 @@ class Cnn14(nn.Module):
         init_layer(self.fc1)
         init_layer(self.fc_audioset)
 
-    def forward(self, input): # TODO: I have deleted mixup part here.
+    def forward(self, input):  # TODO: I have deleted mixup part here.
         """
         Input: (batch_size, data_length)
         """
 
-        x = self.spectrogram_extractor(input) # (batch_size, 1, time_steps, freq_bins)
-        x = self.logmel_extractor(x) # (batch_size, 1, time_steps, mel_bins)
+        x = self.spectrogram_extractor(input)  # (batch_size, 1, time_steps, freq_bins)
+        x = self.logmel_extractor(x)  # (batch_size, 1, time_steps, mel_bins)
 
-        x = x.transpose(1, 3) # (batch_size, freq_bins, time_steps, 1)
+        x = x.transpose(1, 3)  # (batch_size, freq_bins, time_steps, 1)
         x = self.bn0(x)
         x = x.transpose(1, 3)
 
         if self.training:
             x = self.spec_augmenter(x)
-        
+
         x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
         x = F.dropout(x, p=0.2, training=self.training)
         x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
@@ -161,16 +162,16 @@ class Cnn14(nn.Module):
 
 
 class Transfer_Cnn14(nn.Module):
-    def __init__(self, sample_rate, window_size, hop_size, mel_bins, 
-        fmin, fmax, classes_num, freeze_base):
+    def __init__(self, sample_rate, window_size, hop_size, mel_bins,
+                 fmin, fmax, classes_num, freeze_base):
         """Classifier to a new task. Our model actually begins here. """
         super(Transfer_Cnn14, self).__init__()
         # TODO: the class num here is the classnum for CNN14.
         audioset_classes_num = 527
 
         self.base = Cnn14(sample_rate, window_size, hop_size, mel_bins, fmin,
-            fmax, audioset_classes_num)
-        
+                          fmax, audioset_classes_num)
+
         # Transfer
         self.fc_transfer = nn.Linear(2048, classes_num, bias=True)
 
@@ -178,12 +179,12 @@ class Transfer_Cnn14(nn.Module):
             # Freeze AudioSet pretrained layers
             for param in self.base.parameters():
                 param.requires_grad = False
-        
+
         self.init_weight()
-    
+
     def init_weight(self):
         init_layer(self.fc_transfer)
-    
+
     def load_from_pretrain(self, pretrained_checkpoint_path):
         checkpoint = torch.load(pretrained_checkpoint_path)
         self.base.load_state_dict(checkpoint['model'])
@@ -207,7 +208,82 @@ class Transfer_Cnn14(nn.Module):
 
         return outputs
 
-        
+
+import torch.nn as nn
+import torch
+
+
+class ProjectionLayer(nn.Module):
+    """ Classification Layer.
+    """
+
+    def __init__(self, input_size, units):
+        super().__init__()
+        self.input_size = input_size
+        self.units = units
+
+        # Projection
+        self.projection = nn.Linear(input_size, self.units, bias=False)
+
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.projection)
+
+    def forward(self, input):
+        x = input
+        output = self.projection(input)
+        return output
+
+
+class ConcatCLS(nn.Module):
+    """ Classification Layer.
+    """
+
+    def __init__(self, text_input_size, audio_input_size, units, bert_encoder, audio_encoder):
+        super().__init__()
+        self.bert_encoder = bert_encoder
+        self.audio_encoder = audio_encoder
+        self.project_bert = ProjectionLayer(text_input_size, units)
+        self.project_audio = ProjectionLayer(audio_input_size, units)
+        # TODO: tensrodot
+
+    def forward(self, text_input, audio_input):
+        text_output = self.bert_encoder(text_input)
+        audio_output = self.audio_encoder(audio_input)
+        p_bert = self.project_bert(text_output)  # output shape: (batch_size, unit)
+        p_audio = self.project_audio(audio_output)  # shape: (batch_size, unit)
+        # TODO: c = torch.tensordot(a, b, dims=2).cpu()
+        logits = torch.tensordot(p_bert, p_audio, dims=1)
+        # if loss is cross_entropy_with_logits
+        # just return logits
+        # if loss is cross_entropy_without_logits
+        logits = torch.sigmoid(logits)
+        return logits
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_transformers import BertTokenizer
+from pytorch_transformers import BertModel
+
+
+class bertEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.modelInput = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+
+    def forward(self, x):
+        marked_text = "[CLS] " + x + " [SEP]"
+        tokenized_text = self.tokenizer.tokenize(marked_text)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+        tokens_tensor = torch.tensor(indexed_tokens)
+        outputs = self.modelInput(tokens_tensor)
+
+        pooled_output = outputs[1]
+        return pooled_output
+
 
 
 
